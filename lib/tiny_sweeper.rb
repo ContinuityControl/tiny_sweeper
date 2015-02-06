0db6c84222d9f689eea9b9d2deeeed321d2606ff
Dan Bernier <danbernier@gmail.com>
2015-02-06 18:37:24 -0500

    Fold TinySweeper::Cautious into TinySweeper. Default to Cautious.

--- lib/tiny_sweeper.rb
+++ lib/tiny_sweeper.rb
@@ -1,39 +1,37 @@
 module TinySweeper
-  module Cautious
-    module ClassMethods
-      def sweep(field_name, &sweeper)
-        @swept_fields ||= []
+  module ClassMethods
+    def sweep(field_name, &sweeper)
+      @swept_fields ||= []
 
-        if @swept_fields.include?(field_name)
-          raise "Don't sweep #{field_name} twice!"
-        end
-
-        @swept_fields << field_name
-
-        writer_method_name = "#{field_name}=".to_sym
-
-        alias_method "original #{writer_method_name}", writer_method_name
-
-        define_method(writer_method_name) do |value|
-          clean_value = sweeper.call(value)
-          send("original #{writer_method_name}", clean_value)
-        end
+      if @swept_fields.include?(field_name)
+        raise "Don't sweep #{field_name} twice!"
       end
 
-      def sweep_up!(instance)
-        @swept_fields.each do |field|
-          instance.send("#{field}=", instance.send(field))
-        end
+      @swept_fields << field_name
+
+      writer_method_name = "#{field_name}=".to_sym
+
+      alias_method "original #{writer_method_name}", writer_method_name
+
+      define_method(writer_method_name) do |value|
+        clean_value = sweeper.call(value)
+        send("original #{writer_method_name}", clean_value)
       end
     end
 
-    def self.included(base)
-      base.send(:extend, ClassMethods)
+    def sweep_up!(instance)
+      @swept_fields.each do |field|
+        instance.send("#{field}=", instance.send(field))
+      end
     end
+  end
 
-    def sweep_up!
-      self.class.sweep_up!(self)
-    end
+  def self.included(base)
+    base.send(:extend, ClassMethods)
+  end
+
+  def sweep_up!
+    self.class.sweep_up!(self)
   end
 end
 
